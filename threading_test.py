2b519a3b483adc3efc90db5a6b1310c2df6bfd3d
Liam Watts <liam.watts271@gmail.com>
2023-09-21 18:40:42 +0800

    threading_test: add randomised testing for the race condition

--- threading_test.py
+++ threading_test.py
@@ -5,11 +5,15 @@ import time
 from collections import deque
 from concurrent.futures import ThreadPoolExecutor
 from contextlib import contextmanager
+from random import randrange
 from typing import Iterator
+
+from hypothesis import given
+from hypothesis import strategies as st
 
 
 class TerminationController:
-    def __init__(self, duration: int) -> None:
+    def __init__(self, duration: float) -> None:
         self.end_time = time.time() + duration
 
     def should_terminate(self) -> bool:
@@ -47,15 +51,16 @@ class MessageQueue:
             self._message_queue.append(item)
 
 
-def producer(queue: MessageQueue, termination_controller: TerminationController, start_range: int) -> None:
+def producer(queue: MessageQueue, termination_controller: TerminationController, start_range: int, time_scale: float) -> None:
     """Produce some messages"""
     messages = iter(range(start_range, 100_000))
     while not termination_controller.should_terminate():
-        queue.add_item(next(messages))
-        time.sleep(1)
+        for _ in range(randrange(1, 20)):
+            queue.add_item(next(messages))
+        time.sleep(time_scale)
 
 
-def consumer(queue: MessageQueue, termination_controller: TerminationController, processing_time: float) -> None:
+def consumer(queue: MessageQueue, termination_controller: TerminationController, result: list[int], processing_time: float) -> None:
     """Consume messages immediately"""
     while not termination_controller.should_terminate():
         with queue.check_has_item() as has_item:
@@ -70,16 +75,33 @@ def consumer(queue: MessageQueue, termination_controller: TerminationController,
                 print(f"{processing_time=} found an empty queue, after another thread emptied it, which raised {exception=}")
             else:
                 print(f"{processing_time=} Processed: {item} -> {item**2}")
+                result.append(item)
 
 
-def main() -> None:
+def run_experiment(start_val1: int = 10, start_val2: int = 1, time_scale: float = 1) -> list[int]:
     message_queue = MessageQueue(use_locks=True)
-    termination_controller = TerminationController(10)
+    termination_controller = TerminationController(time_scale)
+    result: list[int] = []
     with ThreadPoolExecutor() as executor:
-        executor.submit(producer, message_queue, termination_controller, 10)
-        executor.submit(producer, message_queue, termination_controller, 1)
-        executor.submit(consumer, message_queue, termination_controller, 0.1)
-        executor.submit(consumer, message_queue, termination_controller, 0.01)
+        executor.submit(producer, message_queue, termination_controller, start_val1, time_scale)
+        executor.submit(producer, message_queue, termination_controller, start_val2, time_scale)
+        executor.submit(consumer, message_queue, termination_controller, result, time_scale*0.01)
+        executor.submit(consumer, message_queue, termination_controller, result, time_scale*0.005)
+    return result
+
+
+@given(st.integers(), st.integers())
+def test_race_conditions(start_val1: int, start_val2: int) -> None:
+    """Enforce that the messages are consumed sequentially"""
+    result = run_experiment(start_val1, start_val2, time_scale=0.001)
+    for num in result:
+        if num == start_val1:
+            start_val1 += 1
+        elif num == start_val2:
+            start_val2 += 1
+        else:
+            assert False, f"{num=} not equal to either start values: {start_val1}, {start_val2}"
+
 
 if __name__ == "__main__":
-    main()
+    run_experiment()
