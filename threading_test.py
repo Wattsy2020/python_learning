51efa5e44a79f60de8a66952e2bfaee8874871f3
Liam Watts <liam.watts271@gmail.com>
2023-09-22 15:50:16 +0800

    threading_test: directly implement MessageQueue using lists

--- threading_test.py
+++ threading_test.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import threading
 import time
-from collections import deque
 from concurrent.futures import ThreadPoolExecutor
 from contextlib import contextmanager
 from random import randrange
@@ -22,21 +21,54 @@ class TerminationController:
 
 class MessageQueue:
     def __init__(self, use_locks: bool) -> None:
-        self._message_queue: deque[int] = deque()
+        self._capacity = 8
+        self._message_queue: list[int | None] = [None] * self._capacity
+        self._read_pos = 0
+        self._append_pos = 0
+
         self._read_lock = threading.Lock()
         self._write_lock = threading.Lock()
         self._use_locks = use_locks
 
+    # NOTE: expand_capacity and shrink_capacity need to acquire both locks
+    # (i.e. expand_capacity needs to also acquire read lock, and shrink capacity the write lock)
+    # as reads and writes are both affected by changing self._read_pos and self._append_pos
+
+    def _popleft(self) -> int:
+        """Pop item from the queue. TODO: if queue size (diff between read_pos and append_pos) == floor(half capacity), then shrink the queue to save space"""
+        if self._read_pos == self._append_pos:
+            raise IndexError(f"The queue is empty as {self._read_pos=} == {self._append_pos}")
+        result = self._message_queue[self._read_pos]
+        assert result is not None
+        self._read_pos += 1
+        return result
+    
+    def _expand_capacity(self) -> None:
+        """Double the queue capacity, and remove deleted items"""
+        with self._read_lock:
+            self._capacity *= 2
+            new_message_queue: list[int | None] = [None] * self._capacity
+            new_message_queue[:len(self)] = self._message_queue[self._read_pos:self._append_pos]
+            self._read_pos = 0
+            self._append_pos = len(self)
+
+    def _append(self, item: int) -> None:
+        """Add item, expand the capacity if the maximum capacity is reached"""
+        if self._append_pos == self._capacity:
+            self._expand_capacity()
+        self._message_queue[self._append_pos] = item
+        self._append_pos += 1
+
     @contextmanager
     def check_has_item(self) -> Iterator[bool]:
         if not self._use_locks:
-            yield bool(self._message_queue)
+            yield bool(self)
             return
         with self._read_lock:
-            yield bool(self._message_queue)
+            yield bool(self)
 
     def next_item(self) -> int:
-        return self._message_queue.popleft()
+        return self._popleft()
 
     """
     get_next_item is the ideal model, I separated the has_item and next item functions to cause a race condition
@@ -48,7 +80,13 @@ class MessageQueue:
 
     def add_item(self, item: int) -> None:
         with self._write_lock:
-            self._message_queue.append(item)
+            self._append(item)
+
+    def __len__(self) -> int:
+        return self._append_pos - self._read_pos
+
+    def __bool__(self) -> bool:
+        return len(self) > 0
 
 
 def producer(queue: MessageQueue, termination_controller: TerminationController, start_range: int, time_scale: float) -> None:
