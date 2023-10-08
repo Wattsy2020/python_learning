a3735099da52cd04f2166e7bcababd9eed6ecdf5
Liam Watts <liam.watts271@gmail.com>
2023-10-08 22:24:38 +1100

    pipelines: add Filter class

--- pipelines.py
+++ pipelines.py
@@ -1,8 +1,9 @@
 """Defines a Pipeline class that supports creating pipelines with functions"""
 from __future__ import annotations
 
-from typing import Callable, Generic, TypeVar
+from typing import Callable, Generic, Iterable, TypeVar
 
+T = TypeVar("T")
 T1 = TypeVar("T1")
 T2 = TypeVar("T2")
 T3 = TypeVar("T3")
@@ -17,35 +18,54 @@ class Pipeline(Generic[T1, T2]):
 
     def __ror__(self, other: T1) -> T2:
         return self.f(other)
-    
+
     def __or__(self, other: Callable[[T2], T3]) -> Pipeline[T1, T3]:
         return Pipeline(lambda x: other(self.f(x)))
+
+
+class Filter(Generic[T]):
+    def __init__(self, f: Callable[[T], bool]) -> None:
+        self.f = f
+
+    def __call__(self, arg: T) -> bool:
+        return self.f(arg)
+
+    def __ror__(self, other: Iterable[T]) -> Iterable[T]:
+        yield from (x for x in other if self.f(x))
+
+    def __or__(self, other: Callable[[T], bool]) -> Filter[T]:
+        return Filter(lambda x: self.f(x) and other(x))
 
 
 def main() -> None:
     def list_nums(n: int) -> list[int]:
         return list(range(n))
-    
+
     def sum_nums(nums: list[int]) -> int:
         return sum(nums)
-    
+
     def double(x: int) -> int:
-        return 2*x
-    
-    def filter_even(nums: list[int]) -> list[int]:
-        return list(filter(lambda x: x % 2 == 0, nums))
-    
-    def filter_positive(x: int) -> int:
+        return 2 * x
+
+    def is_even(x: int) -> bool:
+        return x % 2 == 0
+
+    def is_positive(x: int) -> bool:
         return x >= 0
 
     input1 = "5"
-    pipeline = Pipeline(int) | double | list_nums | filter_even | sum_nums
+    pipeline = Pipeline(int) | double | list_nums | sum_nums
     result = input1 | pipeline
     print(result)
 
     inputs = [1, 2, 3, 4]
     result2 = list(map(pipeline, inputs))
     print(result2)
+
+    filters = Filter(is_even) | is_positive
+    inputs2 = [-4, -2, -1, 0, 2, 3, 4]
+    result3 = list(inputs2 | filters)
+    print(result3)
 
 
 if __name__ == "__main__":
